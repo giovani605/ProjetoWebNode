30f92b3fbcbe17c2446428f283380134efce624d
giovani <giovanibossoni@hotmail.com>
2018-09-19 17:33:35 -0300

    Criado conexao com Db postgres Fix gitignore

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var app = express();
 
-app.get('/teste',function(req,res){
+app.get('/teste', function (req, res) {
     console.log("teste funcionando");
     res.status(200).send("oi");
 })
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log("oiii");
 });
+
+const { Pool, Client } = require('pg')
+
+const pool = new Pool({
+    user: 'k2so',
+    host: '200.134.10.32',
+    database: 'k2so',
+    password: '@k2so#',
+    port: 5432,
+});
+pool.query('SELECT * from k2so.teste', (err, res) => {
+    console.log(res.rows);
+    pool.end();
+})
+
