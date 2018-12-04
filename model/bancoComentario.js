224968593c045f38fa949b344aea89294668fb10
José Moscardi da Silva Junior <jmoscajr@gmail.com>
2018-12-04 18:00:21 -0200

    bancoComentario com querys prontas

--- model/bancoComentario.js
+++ model/bancoComentario.js
@@ -19,7 +19,7 @@ function inserirComentarioPrato(idUsuario, idPrato, nota, comentario, data, call
     });
 }
 
-function inserirComentarioPrato(idUsuario, idPrato, nota, comentario, data, callback) {
+function inserirComentarioRestaurante(idUsuario, idPrato, nota, comentario, data, callback) {
     var query = "insert into k2so.comentarios_restaurantes(cliente_idusuario, restaurante_idrestaurante, nota, comentario, data) " + 
     "values (" + idUsuario + ", " + idPrato + ", " + nota + ", '" + comentario + "', '" + data + "');";    
     
@@ -37,8 +37,10 @@ function inserirComentarioPrato(idUsuario, idPrato, nota, comentario, data, call
     });
 }
 
-function buscarUltimosComentariosPratos(idPrato, callback) {
-    var query = "select * from pratos  where idpratos = " + idPrato;
+function buscarTodosComentariosPratos(idPrato, callback) {
+    var query = "select usuario.idusuario, usuario.nome, comentarios_pratos.* from usuario, comentarios_pratos " + 
+    "where comentarios_pratos.cliente_idusuario = usuario.idusuario and comentarios_pratos.pratos_idpratos = " + idPrato + 
+    " order by comentarios_pratos.idcomentarios_pratos desc;";
     conexao.query(query, (err, res) => {
         if (err) {
             console.log("problemas : ");
@@ -50,9 +52,74 @@ function buscarUltimosComentariosPratos(idPrato, callback) {
             callback(res.rows, false);
             return;
         }
-        console.log("Selecionado pratos : ");
+        console.log("Selecionado comentários do prato: ");
         console.log(res.rows);
         callback(res.rows, true);
         return;
     });
 }
+
+function buscarTodosComentariosRestaurante(idRestaurante, callback) {
+    var query = "select usuario.idusuario, usuario.nome, comentarios_restaurantes.* from usuario, comentarios_restaurantes " +
+    " where comentarios_restaurantes.cliente_idusuario = usuario.idusuario and comentarios_restaurantes.restaurante_idrestaurante = " + idRestaurante +
+    " order by comentarios_restaurantes.idcomentario desc;";
+    conexao.query(query, (err, res) => {
+        if (err) {
+            console.log("problemas : ");
+            console.log(err);
+            callback(err, false);
+            return;
+        }
+        if (res.rows.length == 0) {
+            callback(res.rows, false);
+            return;
+        }
+        console.log("Selecionado comentários do restaurante: ");
+        console.log(res.rows);
+        callback(res.rows, true);
+        return;
+    });
+}
+
+function buscarNotaRestaurante(idRestaurante, callback) {
+    var query = "select  coalesce(avg(comentarios_restaurantes.nota),0) " + 
+    "from comentarios_restaurantes where comentarios_restaurantes.restaurante_idrestaurante = " + idRestaurante + ";";
+    conexao.query(query, (err, res) => {
+        if (err) {
+            console.log("problemas : ");
+            console.log(err);
+            callback(err, false);
+            return;
+        }
+        if (res.rows.length == 0) {
+            callback(res.rows, false);
+            return;
+        }
+        console.log("Selecionado comentários do restaurante: ");
+        console.log(res.rows);
+        callback(res.rows, true);
+        return;
+    });
+}
+
+function buscarNotaPrato(idPrato, callback) {
+    var query = "select coalesce(avg(comentarios_pratos.nota),0) " + 
+    "from comentarios_pratos where comentarios_pratos.pratos_idpratos = " + idPrato + ";";
+    conexao.query(query, (err, res) => {
+        if (err) {
+            console.log("problemas : ");
+            console.log(err);
+            callback(err, false);
+            return;
+        }
+        if (res.rows.length == 0) {
+            callback(res.rows, false);
+            return;
+        }
+        console.log("Selecionado comentários do restaurante: ");
+        console.log(res.rows);
+        callback(res.rows, true);
+        return;
+    });
+}
+
